7915965008fe7c5bb40e2f133427d6aaabbd84ac
Shubham2503 <46487179+Shubham2503@users.noreply.github.com>
2020-04-10 23:17:45 +0530

    added files

---
+++ 3D_Visualization_Of_Function/Assets/Scripts/Graph.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    public Transform pointPrefab;
+    void Awake()
+    {
+        Transform point = Instantiate(pointPrefab);
+        point.localPosition = Vector3.right;
+
+        point = Instantiate(pointPrefab);
+        point.localPosition = Vector3.right * 2f;
+    }
+}
